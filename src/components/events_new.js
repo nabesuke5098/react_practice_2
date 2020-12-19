17a6ac591c16a4ce3c980676bd7d3edcef84eda4
nabesuke <nabesuke5098@gmail.com>
2020-12-19 16:42:29 +0900

    [modified]events-new

---
+++ src/components/events_new.js
@@ -0,0 +1,17 @@
+import React, { Component, Fragment } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom'
+
+// import { postEvent } from '../actions';
+
+class EventsNew extends Component {
+
+  render() {
+    return (
+      <Fragment>
+        <p>foo</p>
+      </Fragment>
+    )
+  }
+}
+export default connect(null, null)(EventsNew)
